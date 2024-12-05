34e745c1b64c5e6ca6eca0035a8ebc00be9546fd
Papigalla <ramakullaipapigalla2001@gmail.com>
2024-12-05 15:31:24 +0530

    project wirritng

---
+++ src/main/java/com/example/FirstGitHubProject/FirstGitHubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstGitHubProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitHubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGitHubProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FirstGitHubProject/FirstGitHubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstGitHubProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGitHubProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
